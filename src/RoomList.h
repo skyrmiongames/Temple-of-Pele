fef703176b2675d17b8d482dd30e87955e59ab35
Stuart Irwin <stuin01@gmail.com>
2019-04-13 16:35:23 -0700

    Textures and base headers

---
+++ src/RoomList.h
@@ -0,0 +1,18 @@
+#include <Rect.hpp>
+#include "Node.h"
+#include "Room.h"
+
+/*
+ * Created by Stuart Irwin on 4/13/2019.
+ * Manages list of rooms through update cycle
+ */
+
+class RoomList {
+private:
+	const int SIZE = 1;
+	(Room*)[] rooms = new Room[SIZE];
+
+public:
+	RoomList();
+
+}

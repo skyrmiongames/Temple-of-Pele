3cb15f95a6718c720ae6dbeabfcf81ddf705a500
Stuart Irwin <stuin01@gmail.com>
2019-04-17 15:44:05 -0700

    Fixed statis texture stuff

--- Game/src/Player.cpp
+++ Game/src/Player.cpp
@@ -19,7 +19,7 @@ void Player::eightWayMovement()
 		this->setTexture(textures::playerMoveUp);
 		move(North);
 	}
-	else if (sf::Keyboard::S) // down
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) // down
 	{
 		this->setTexture(textures::playerMoveDown);
 		move(South);
--- Game/src/textures.h
+++ Game/src/textures.h
@@ -3,19 +3,7 @@
 
 class textures
 {
-  public:
-	  static void load()
-	  {
-		  playerIdleDown.loadFromFile("resources/Player_IdleDown.png");
-		  playerIdleUp.loadFromFile("resources/Player_IdleUp.png");
-		  playerIdleSide.loadFromFile("resources/Player_IdleSide.png");
-		  playerMoveDown.loadFromFile("resources/Player_WalkDown.png");
-		  playerMoveUp.loadFromFile("resources/Player_WalkUp.png");
-		  playerMoveSide.loadFromFile("resources/Player_WalkSide.png");
-
-		  healthSpriteTexture.loadFromFile("resources/Health_HeartsGif.png");
-	  };
-
+public:
 	static sf::Texture playerIdleDown;
 	static sf::Texture playerIdleUp;
 	static sf::Texture playerIdleSide;
@@ -24,4 +12,6 @@ class textures
 	static sf::Texture playerMoveSide;
 
 	static sf::Texture healthSpriteTexture;
+
+	static void load();
 };

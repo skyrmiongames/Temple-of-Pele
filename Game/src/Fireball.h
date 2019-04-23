af83c739144032ea80186feb2f6109bf9f713897
Duncan Gibson <legowerewolf@gmail.com>
2019-04-23 15:17:34 -0700

    Add collision and update functions

--- Game/src/Fireball.h
+++ Game/src/Fireball.h
@@ -5,12 +5,26 @@
 
 class Fireball : public Enemy {
 public:
-	Fireball(sf::Vector2f location, float angle, float speed = 0.5) : Enemy() {
+	Fireball(sf::Vector2f location, float _angle, float _speed = 0.5) : Enemy() {
 		setPosition(location);
-		setTexture(textures->fireball);
+		setTexture(textures->healthSpriteTexture); // TODO: needs own texture file
 	}
 	~Fireball();
 
+	void update(double time) {
+		move(angle, speed, true);
+	}
+
+	void collide(Node* object) {
+		Enemy::collide(object); //allow the player to block fireballs
+
+		if (object->get_layer() == WALL) { // walls kill fireballs
+			set_delete();
+		}
+	};
+
 private:
+	float angle;
+	float speed;
 
 };

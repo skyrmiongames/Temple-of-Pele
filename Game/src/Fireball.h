e923a12a53d442ef0e694a45b345e766b6df03e2
Duncan Gibson <legowerewolf@gmail.com>
2019-04-23 15:29:51 -0700

    Fireballs now select an angle based on the position of the player *at construction time* if not provided.

--- Game/src/Fireball.h
+++ Game/src/Fireball.h
@@ -2,14 +2,21 @@
 
 #include "Enemy.h"
 #include "textures.h"
+#include "Node.h" // for access to playerPos
 
 class Fireball : public Enemy {
 public:
-	Fireball(sf::Vector2f location, float _angle, float _speed = 0.5) : Enemy() {
+	Fireball(
+		sf::Vector2f location, 
+		float _angle = 1.30357, // using Conway's constant for an interesting value that will probably never occur
+		float _speed = 0.5
+	) : Enemy() {
 		setPosition(location);
+		angle = _angle == 1.30357 ? angleTo(location, playerPos): _angle;
+
 		setTexture(textures->healthSpriteTexture); // TODO: needs own texture file
 	}
-	~Fireball();
+	~Fireball() {}
 
 	void update(double time) {
 		move(angle, speed, true);

8ebc953ea79a8e6201410e79fde23f4357dcc34f
Stuart Irwin <stuin01@gmail.com>
2019-04-24 15:31:06 -0700

    Added tests and removed extra things

---
+++ Tests/src/TestNode.hpp
@@ -0,0 +1,24 @@
+#include "../../Game/src/Node.h"
+
+/*
+ * Created by Stuart Irwin on 4/24/2019.
+ * Node to return tests and checks
+ */
+
+namespace Tests {
+	class TestNode : public Node {
+	public:
+		bool collided = false;
+		bool updated = false;
+
+		TestNode() : Node(PLAYER) {}
+
+		void update(double time) {
+			updated = true;
+		}
+
+		void collide(Node *object) {
+			updated = true;
+		}
+	}
+}

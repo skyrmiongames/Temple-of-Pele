3cd524318e2f2d4dae3809853e3e177405ffd528
Duncan Gibson <legowerewolf@gmail.com>
2019-04-24 15:46:29 -0700

    Fix tests as best I can

--- Tests/src/Tests.cpp
+++ Tests/src/Tests.cpp
@@ -4,12 +4,13 @@
 #include "../../Game/src/enums.h"
 
 #include "../../Game/src/UpdateList.h"
+#include "TestNode.hpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace Tests {
 	TEST_CLASS(Helpers) {
-	public:
+public:
 
 	TEST_METHOD(oneof_positive) {
 		Assert::IsTrue(oneof(1, 3, 1, 2, 3));
@@ -17,60 +18,60 @@ namespace Tests {
 		Assert::IsTrue(oneof(North, 3, Northeast, North, Southeast));
 	}
 
-		TEST_METHOD(oneof_negative) {
-			Assert::IsFalse(oneof(1, 3, 2, 3, 4));
-			Assert::IsFalse(oneof(North, 3, Northeast, East, Southeast));
-		}
+	TEST_METHOD(oneof_negative) {
+		Assert::IsFalse(oneof(1, 3, 2, 3, 4));
+		Assert::IsFalse(oneof(North, 3, Northeast, East, Southeast));
+	}
 
-		TEST_METHOD(angleCalc) {
-			Assert::AreEqual(angleTo(sf::Vector2f(0, 0), sf::Vector2f(5,0)), 0.0f);
-		}
+	TEST_METHOD(angleCalc) {
+		Assert::AreEqual(angleTo(sf::Vector2f(0, 0), sf::Vector2f(5, 0)), 0.0f);
+	}
 	};
 
 	TEST_CLASS(UpdateList) {
-	public:
-		TEST_METHOD(node_update) {
-			//Create testing node
-			Node *testNode = new testNode;
-			UpdateList::add_node(testNode);
+public:
+	TEST_METHOD(node_update1) {
+		//Create testing node
+		TestNode* testNode = new TestNode();
+		::UpdateList::add_node(testNode);
 
-			//Check start conditions
-			Assert::IsFalse(testNode->collided);
-			Assert::IsFalse(testNode->updated);
+		//Check start conditions
+		Assert::IsFalse(testNode->collided);
+		Assert::IsFalse(testNode->updated);
 
-			//Run single update
-			UpdateList::update(5);
+		//Run single update
+		::UpdateList::update(5);
 
-			//Check final condition
-			Assert::IsFalse(testNode->collided);
-			Assert::IsTrue(testNode->updated);
-		}
+		//Check final condition
+		Assert::IsFalse(testNode->collided);
+		Assert::IsTrue(testNode->updated);
+	}
 
-		TEST_METHOD(node_update) {
-			//Create testing node 1
-			Node *testNode1 = new testNode;
-			testNode1->setPosition(40, 40);
-			UpdateList::add_node(testNode1);
+	TEST_METHOD(node_update2) {
+		//Create testing node 1
+		TestNode* testNode1 = new TestNode();
+		testNode1->setPosition(40, 40);
+		::UpdateList::add_node(testNode1);
 
-			//Create testing node 2
-			Node *testNode2 = new testNode;
-			testNode2->setPosition(50, 50);
-			UpdateList::add_node(testNode1);
+		//Create testing node 2
+		TestNode* testNode2 = new TestNode();
+		testNode2->setPosition(50, 50);
+		::UpdateList::add_node(testNode1);
 
-			//Check start conditions
-			Assert::IsFalse(testNode1->collided);
-			Assert::IsFalse(testNode1->updated);
-			Assert::IsFalse(testNode2->collided);
-			Assert::IsFalse(testNode2->updated);
+		//Check start conditions
+		Assert::IsFalse(testNode1->collided);
+		Assert::IsFalse(testNode1->updated);
+		Assert::IsFalse(testNode2->collided);
+		Assert::IsFalse(testNode2->updated);
 
-			//Run single update
-			UpdateList::update(5);
+		//Run single update
+		::UpdateList::update(5);
 
-			//Check final conditions
-			Assert::IsTrue(testNode1->collided);
-			Assert::IsTrue(testNode1->updated);
-			Assert::IsTrue(testNode2->collided);
-			Assert::IsTrue(testNode2->updated);
-		}
+		//Check final conditions
+		Assert::IsTrue(testNode1->collided);
+		Assert::IsTrue(testNode1->updated);
+		Assert::IsTrue(testNode2->collided);
+		Assert::IsTrue(testNode2->updated);
+	}
 	};
 }
